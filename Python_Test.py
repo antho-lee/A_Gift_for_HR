8c9c088c7d7ef77e1a8ee82a82743d943f6404d5
antho-lee <150319887+antho-lee@users.noreply.github.com>
2024-09-22 23:45:48 +0800

    Add files via upload

---
+++ Python_Test.py
@@ -0,0 +1,42 @@
+# Question 1
+'''
+*******
+*****
+***
+*
+'''
+# use only 2 lines of code to print out the asterisk (*) in above pattern:
+def question1():
+    pass
+
+# Question 2
+def question2():
+    # Modify the below line
+    test_list = []
+    # so that the below line prints out "Apple"
+    print(test_list[2]["fruits"][0])
+
+# Question 3 
+def question3():
+# optimize this algorithm:
+    for i in range(10):
+        if i % 2 == 0:
+            print(int(i/2))
+        if i % 2 == 1:
+            print(int((i-1)/2))
+
+# Question 4
+class classA:
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
+        self.attr = "congrats"
+
+# create "classA" and print out "congrats" 
+def question4():
+    pass
+
+question1()
+question2()
+question3()
+question4()
